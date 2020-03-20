48d31ca026e039649ebcff42917ed99e3b76150e
SimonvanLoon <s.p.c.a.van.loon@student.rug.nl>
2020-03-20 15:51:27 +0100

    final project i.r.m.

--- extract_articles.py
+++ extract_articles.py
@@ -3,16 +3,11 @@ import re
 import pandas as pd
 
 df = pd.read_csv('articles1.csv', sep=',', header=0)
-news_sources = df['publication'].value_counts()
-date_range = df['month'].value_counts()
-print(df.shape) 
-years_before_2017 = df.loc[df['year'] < 2017.0, :]
-suitable_article_dates = years_before_2017.loc[df['month'] < 11.0, :]
 suitable_article_dates = df.loc[("2015-06-18" <= df['date'] ) & (df['date'] <= "2016-11-07"), :] 
-print(suitable_article_dates['date'].describe())
 left_wing_papers = suitable_article_dates.loc[(df['publication'] == 'New York Times') | (df['publication'] == 'CNN'), :]
 right_wing_papers = suitable_article_dates.loc[df['publication'] == 'Breitbart', :]
 p = re.compile("\bTrump(\'s)*[\s\.]{1}")
 left_wing_trump = left_wing_papers.loc[left_wing_papers['title'].str.contains(r"\bTrump(\'s)*[\s\.]{1}", regex=True), :]
 right_wing_trump = right_wing_papers.loc[right_wing_papers['title'].str.contains(r"\bTrump(\'s)*[\s\.]{1}", regex=True), :]
-print(left_wing_trump['date'].describe())
+print(left_wing_trump)
+print(right_wing_trump)
--- final_project.sh
+++ final_project.sh
@@ -2,4 +2,14 @@
 
 # because of the great file size of each of the articles, we will analyze only the csv file with 
 
-cat articles3.csv | grep -o 'Trump' | wc -l
+#echo "There are `grep -P 'positive\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | wc -l` words with a positive connotation,
+#and `grep -P 'negative\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | wc -l` words with a negative connotation."
+
+#grep -P '[(posi)(nega)]tive\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt > short_lexicon.txt
+
+grep -P 'positive\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | cut -f1 > positive_words.txt
+
+grep -P 'negative\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | cut -f1 > negative_words.txt
+
+grep -vf negative_words.txt positive_words.txt > unique_pos_words.txt # select all the lines from positive_words.txt that are not included in the negative word list. 
+grep -vf positive_words.txt negative_words.txt > unique_neg_words.txt # select all the lines from negative_words.txt that are not included in the positive word list. 
