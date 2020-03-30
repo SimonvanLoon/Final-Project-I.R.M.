f298a3657cd794f7b57951de579ef62a95da626d
SimonvanLoon <s.p.c.a.van.loon@student.rug.nl>
2020-03-30 23:08:36 +0200

    final project

--- extract_articles.py
+++ extract_articles.py
@@ -8,69 +8,58 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 
 def extract_articles():
     df = pd.read_csv('articles1.csv', sep=',', header=0)
-    suitable_article_dates = df.loc[(
-        "2016-04-20" <= df['date']) & (df['date'] <= "2016-11-07"), :]
-    left_wing_papers = suitable_article_dates.loc[(
+    suitable_article_dates = df.loc[(  # select suitable article dates
+        "2016-05-10" <= df['date']) & (df['date'] <= "2016-11-07"), :]
+    left_wing_papers = suitable_article_dates.loc[(  # select articles with either The New York Times or CNN as publisher
         df['publication'] == 'New York Times') | (df['publication'] == 'CNN'), :]
-    right_wing_papers = suitable_article_dates.loc[df['publication']
+    right_wing_papers = suitable_article_dates.loc[df['publication']  # select articles with Breitbart as publisher
                                                    == 'Breitbart', :]
     p = re.compile(r"\bTrump(\'s)*[\s\.]{1}")
-    left_wing_trump = left_wing_papers.loc[left_wing_papers['title'].str.contains(
-        r"\bTrump(\'s)*[\s\.]{1}", regex=True), :].sort_values(by='date')
-    right_wing_trump = right_wing_papers.loc[right_wing_papers['title'].str.contains(
-        r"\bTrump(\'s)*[\s\.]{1}", regex=True), :].sort_values(by='date')
+    left_wing_trump = left_wing_papers.loc[left_wing_papers['title'].str.contains(  # select articles with contain the regex in the title column
+        r"\bTrump(\'s)*[\s\.]{1}", regex=True), :].sort_values(by='date').head(600)
+    right_wing_trump = (right_wing_papers.loc[right_wing_papers['title'].str.contains(
+        r"\bTrump(\'s)*[\s\.]{1}", regex=True), :].sort_values(by='date')).loc[::2, :].head(600)  # take every 2nd article of the Breitbart articles
     return [left_wing_trump, right_wing_trump]
 
 
-def count_sentiment_words(dataframe):
+def count_sentiment_articles(dataframe):
     pos_words = open("unique_pos_words.txt", "r").read()
     neg_words = open("unique_neg_words.txt", "r").read()
     punctuation_marks = ".,?!:;\'\"“”’‘—()@#[]"
-    pos_word_count = 0
-    neg_word_count = 0
+    pos_article_count = 0
+    neg_article_count = 0
     total_word_count = 0
     for article in dataframe['content'].tolist():
+        pos_word_count = 0
+        neg_word_count = 0
         for mark in punctuation_marks:
             article = article.replace(mark, "")
         for word in article.split():
-            total_word_count += 1
             word = word.lower()
             if word in pos_words:
                 pos_word_count += 1
             if word in neg_words:
                 neg_word_count += 1
-            total_word_count += 1
-            if total_word_count == 537134:
-                return [total_word_count, pos_word_count, neg_word_count]
+        if pos_word_count > neg_word_count:
+            pos_article_count += 1
+        if neg_word_count > pos_word_count:
+            neg_article_count += 1
+    return [pos_article_count, neg_article_count]
 
 
 def main():
     extracted_articles_df = extract_articles()
-    left_wing_sentiments = count_sentiment_words(extracted_articles_df[0])
-    right_wing_sentiments = count_sentiment_words(extracted_articles_df[1])
+    left_wing_sentiments = count_sentiment_articles(extracted_articles_df[0])
+    right_wing_sentiments = count_sentiment_articles(extracted_articles_df[1])
     print()
     print(
-        "Total amount of words in left-wing articles: {0}\n\
-Amount of words with positive sentiment: {1}\n\
-Amount of words with negative sentiment: {2}\n\
-There are {3} more negative words than positive words \
-in the left-wing articles." .format(
+        "Total amount of left-wing articles about Trump with an overall positive sentiment: {0}\n\
+Total amount of right-wing articles about Trump with an overall positive sentiment: {1}\n\n\
+Total amount of left-wing articles about Trump with an overall negative sentiment: {2}\n\
+Total amount of right-wing articles about Trump with an overall negative sentiment: {3}".format(
             left_wing_sentiments[0],
-            left_wing_sentiments[1],
-            left_wing_sentiments[2],
-            left_wing_sentiments[2] -
-            left_wing_sentiments[1]))
-    print("\n----------------------------------------------------------------------\n")
-    print(
-        "Total amount of words in right-wing articles: {0}\n\
-Amount of words with positive sentiment: {1}\n\
-Amount of words with negative sentiment: {2}\n\
-There are {3} more negative words than positive words \
-in the right-wing articles.".format(
             right_wing_sentiments[0],
-            right_wing_sentiments[1],
-            right_wing_sentiments[2],
-            right_wing_sentiments[2] -
+            left_wing_sentiments[1],
             right_wing_sentiments[1]))
 
 
--- final_project.sh
+++ final_project.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# S.P.C.A. van Loon / 3515710
+# 30-02-2020
+# final_project.sh
 
-# because of the great file size of each of the articles, we will analyze only the csv file with 
+# Instructions: Make sure that this bash script is run in the same
+# directory as 'all-the-news.zip', 'NRC-Emotion-Lexicon.zip' and 'extract_articles.py'.
+# Start the script like this: $: bash final_project.sh
 
-#echo "There are `grep -P 'positive\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | wc -l` words with a positive connotation,
-#and `grep -P 'negative\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | wc -l` words with a negative connotation."
-
-#grep -P '[(posi)(nega)]tive\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt > short_lexicon.txt
-
-grep -P 'positive\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | cut -f1 > positive_words.txt
-
-grep -P 'negative\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | cut -f1 > negative_words.txt
-
+unzip all-the-news.zip -x
+unzip NRC-Emotion-Lexicon.zip -x
+mv NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt
+grep -P 'positive\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | cut -f1 > positive_words.txt # select al words with a positive score of 1
+grep -P 'negative\t1' NRC-Emotion-Lexicon-Wordlevel-v0.92.txt | cut -f1 > negative_words.txt # select al words with a negative score of 1
 grep -vf negative_words.txt positive_words.txt > unique_pos_words.txt # select all the lines from positive_words.txt that are not included in the negative word list. 
 grep -vf positive_words.txt negative_words.txt > unique_neg_words.txt # select all the lines from negative_words.txt that are not included in the positive word list. 
+
+python3 extract_articles.py
